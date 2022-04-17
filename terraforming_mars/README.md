d469172e698477306ce1703765065a3cc34bb5bb
Gabriel Metola <metolag@gmail.com>
2022-04-17 22:09:44 +0200

    starting with terraforming mars

---
+++ terraforming_mars/README.md
@@ -0,0 +1,4 @@
+Let's start by creating a mini version of the board game.
+Then I'll try to add IA.
+
+IF you are interesed in this project, just contact me. We can work together.
---
+++ terraforming_mars/game_elements.py
@@ -0,0 +1,125 @@
+"""
+    A general list of elements in the game. I will organize and subdivide later.
+"""
+RESOURCES = ['megacredits', 'steel', 'titanium', 'plants', 'energy', 'heat']
+STANDARD_PJ = ['sell patent', 'power plant', 'asteroid', 'aquifier', 'greenery',' city']
+
+class game_elem():
+    def __init__(self, name, cost) -> None:
+        self.name = name
+        self.cost = cost
+
+## RESOURCES
+
+
+class Action():
+    def __init__(self) -> None:
+        pass
+    pass
+
+class Card(game_elem):
+    def __init__(self, 
+                name, cost,
+                requirements={},
+                consequences={}
+                ) -> None:
+        super().__init__(name, cost)
+        self.playable = False
+        self.requirements = requirements
+        self.consequences = consequences
+
+
+class Resource(game_elem):
+    def __init__(self, name) -> None:
+        self.name = name
+        self.qty = 0
+        if self.name in ['plant', 'heat']:
+            self.terraformable = True
+        else:
+            self.terraformable = False
+
+        if self.name in ['steel', 'titanium']:
+            self.convertible = True
+        else:
+            self.convertible = False
+
+
+class Player():
+    def __init__(self) -> None:
+        self.corporation = 'basic'
+        self.vict_points = 0
+        self.terr_points = 20
+        self.resources = dict.fromkeys(RESOURCES, 0)
+        self.res_production = dict.fromkeys(RESOURCES, 0)
+        
+        # turn
+        self.turn_active = False
+        self.actions_left = 2
+        self.log = []
+    pass
+
+
+class Milestones_and_Awards():
+    def __init__(self, name, cost) -> None:
+        self.name = name
+        self.cost = cost
+        self.owner = None
+        self.aspirant_ranking = []  # list of players by order
+        self.activated = False
+
+
+class StandardProject():
+    def __init__(self) -> None:
+        pass
+    pass
+
+
+class Awards():
+    def __init__(self) -> None:
+        pass
+    pass
+
+
+## MARS
+
+class Mars():
+    def __init__(self) -> None:
+        pass
+    pass
+
+
+class TerraformElement():
+    def __init__(self, name, max, min, step) -> None:
+        self.name = name
+        self.max = max
+        self.min = min
+        self.step = step
+
+    pass
+
+
+class Tile():
+    def __init__(self) -> None:
+        pass
+    pass
+
+
+## GAME PHASES
+
+class SetupGame():
+    def __init__(self) -> None:
+        pass
+    pass
+
+
+class Turn():
+    def __init__(self) -> None:
+        pass
+    pass
+
+
+class EndGame():
+    def __init__(self) -> None:
+        pass
+    pass
+
